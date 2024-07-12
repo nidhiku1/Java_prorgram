2141e9f4041b8ba2ea89e931b9e6817812a97c4c
Nidhi <nidhik0096@gmail.com>
2024-07-12 08:20:31 +0530

    New file is added

--- src/Java_package/Java_Demo.java
+++ src/Java_package/Java_Demo.java
@@ -1,2 +1,8 @@
-package Java_package;public class Java_Demo {
+package Java_package;
+
+public class Java_Demo {
+
+    public static void main(String[] args) {
+System.out.println("hello world");
+    }
 }
