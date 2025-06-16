56a228cef3d3433b09abcec051e881fa94970dee
nidhiku1 <54815867+nidhiku1@users.noreply.github.com>
2025-06-16 23:40:39 +0530

    New changes done2
    New changes done2

--- src/Java_package/Java_Demo.java
+++ src/Java_package/Java_Demo.java
@@ -1,12 +1,14 @@
 package Java_package;
 
 public class Java_Demo {
-
     public static void main(String[] args) {
-System.out.println("hello world");
-System.out.println("Hello Word 2");
-int i=0;
-int j=0;
-
+        System.out.println("hello world");
+        System.out.println("Hello Word 2");
+        int i = 0;
+        int j = 0;
+        public void cleanUp ()
+        {
+            System.out.println("New branch");
+        }
     }
 }
