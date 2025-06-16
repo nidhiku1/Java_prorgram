810cc405ce9e32fe50c4e49378ae9077904069d8
nidhiku1 <54815867+nidhiku1@users.noreply.github.com>
2025-06-16 22:38:12 +0530

    Java program
    Java program
    
    Co-Authored-By: nidhi170 <113664011+nidhi170@users.noreply.github.com>

--- src/Java_package/Java_Demo.java
+++ src/Java_package/Java_Demo.java
@@ -3,6 +3,7 @@ package Java_package;
 public class Java_Demo {
 
     public static void main(String[] args) {
-System.out.println("hello world");;;
+System.out.println("hello world");
+System.out.println("jsh");
     }
 }
