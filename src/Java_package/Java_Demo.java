e85c8c9f9dd0c74c55000a57199c14e396335215
nidhiku1 <54815867+nidhiku1@users.noreply.github.com>
2025-06-16 22:49:38 +0530

    aa

--- src/Java_package/Java_Demo.java
+++ src/Java_package/Java_Demo.java
@@ -4,6 +4,6 @@ public class Java_Demo {
 
     public static void main(String[] args) {
 System.out.println("hello world");
-System.out.println("Hello Word 2");
+System.out.println("Hello Word 2");;;
     }
 }
